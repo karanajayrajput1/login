0723d52f9412eeb2c2c27664ae5c9549ec45ca29
karanajayrajput1 <115364131+karanajayrajput1@users.noreply.github.com>
2022-10-14 18:20:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         alert("logged out");
-        $(".data").css("display","none");
         $(".g-sighin2").css("display","block");
+        $(".data").css("display","none");
     });
 }
